d27fd3b4b7b6de1a0379c2a5e44f70008421784a
DavidG <datog309@gmail.com>
2021-05-15 23:16:34 +0400

    removed example test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -17,27 +17,6 @@ const Book = require("../models/Book");
 chai.use(chaiHttp);
 
 suite('Functional Tests', function () {
-
-    /*
-    * ----[EXAMPLE TEST]----
-    * Each test should completely test the response of the API end-point including response status code!
-    */
-    test('#example Test GET /api/books', function (done) {
-        chai.request(server)
-            .get('/api/books')
-            .end(function (err, res) {
-                assert.equal(res.status, 200);
-                assert.isArray(res.body, 'response should be an array');
-                assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
-                assert.property(res.body[0], 'title', 'Books in array should contain title');
-                assert.property(res.body[0], '_id', 'Books in array should contain _id');
-                done();
-            });
-    });
-    /*
-    * ----[END of EXAMPLE TEST]----
-    */
-
     let id1, id2;
     const id = new ObjectID();
     this.beforeEach(async () => {
